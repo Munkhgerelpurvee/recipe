7a88ac82ff64de18521515c045b06c3473ad2ff6
Munkhgerelpurvee <purveemunkhgerel@gmail.com>
2025-01-30 13:11:11 +0800

    js-lesson114 Axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import query from './model/Search';
 import { add,multiply as m, id } from'./view/searchView';
+import axios from 'axios';
 
 
 
@@ -20,3 +21,30 @@ console.log('Хайлт : ' + query);
 console.log('Хоёр тооны нийлбэр бол : ' + add(6,7));
 console.log('Хоёр тооны үржвэр бол : ' + m(6,5));
 console.log('Id is : ' + id);
+/*
+axios ашиглаад бүх жор хайгаад түүнийг console-руу бичдэг функц бичье
+*/
+
+async function doSearch(search) {
+    try {
+
+        
+        let result = await axios('https://forkify-api.herokuapp.com/api/search?q=' + search);
+        // console.log(result);
+        const recipes = result.data.recipes;
+        console.log(recipes);
+        // Одоо энэ 28 массив дотроос алв нэг жорыг нь орцтой нь хамт гаргая авъя
+        
+    
+        result = await axios('https://forkify-api.herokuapp.com/api/get?rId=' + recipes[5].recipe_id
+        );
+    
+        console.log(result);
+
+
+    } catch(error) {
+        alert('Асуудал гарлаа : ' + error);
+    }
+    
+};
+doSearch('pizza');
